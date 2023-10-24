919dd86c2f2d46dcfebe961e4d9ef0d89668b926
Yutak_Seo <soano2428@gmail.com>
2023-10-24 12:47:41 +0900

    23.10.24

--- main.py
+++ main.py
@@ -18,10 +18,11 @@ from cornerDetector import *
 from Sensor import *
 from createArray import *
 from createCordinate import *
+import time
 
 
-
-map_data = rectangle_140by140
+start = time.time()
+map_data = stair_140by140
 
 
 coners = None
@@ -30,11 +31,11 @@ corners_position = VisualTool()
 coners = createCordinate(binary_corner(dst))
 
 
-print(coners)
 
 for i in range(len(coners)):
-    test_instance = Sensor(map_data, coners[i], 30)
-    test_instance.deploy_sensor()
+    sensor_instance = Sensor(map_data, coners[i], 6)
+    sensor_instance.deploy_sensor()
+
 
 
 visual_tool  = VisualTool()
