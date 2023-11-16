812366c8af83e621bd76b7fca59246c0dec9bb45
Yutak_Seo <soano2428@gmail.com>
2023-11-16 15:20:15 +0900

    Revert "23.11.16"
    This reverts commit 2d1afa8c9359033e49864f8c3cd0c4609cadf5a6.

---
+++ Checker/createArray.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def make_array(data):
+    matrix = np.array(data)
+    return matrix
+
+def create_padding(matrix):
+    filled_matrix = matrix.copy()
+    filled_matrix[0, :] = 0  # 첫 번째 행
+    filled_matrix[-1, :] = 0  # 마지막 행
+    filled_matrix[:, 0] = 0  # 첫 번째 열
+    filled_matrix[:, -1] = 0  # 마지막 열
+    return filled_matrix
+
+def visual_array(title:str, data):
+    matrix = np.array(data)
+    rows, cols = matrix.shape
+    #사용할 컬러는 회색과 흰색
+    cmap_custom = plt.cm.colors.ListedColormap(['gray', 'white'])
+    
+    #행렬을 이미지(그래프)로 표시
+    plt.imshow(matrix, cmap=cmap_custom, interpolation='nearest', extent=[0, cols, rows, 0], origin='upper')
+    plt.title(title)
+    
+    #격자를 설정, 간격은 1
+    plt.xticks(np.arange(0, cols, step=1))
+    plt.yticks(np.arange(0, rows, step=1))
+    #x축을 상단에 고정
+    plt.tick_params(axis='x', which='both', bottom=False, top=True, labelbottom=False, labeltop=True)
+    #격자의 스타일
+    plt.grid(which='both', color='black', linestyle='-', linewidth=0.5)
+    
+    plt.show()
+    return matrix
+    
+def binary_corner(corner_image):
+    max_value = np.max(corner_image)
+    binary_image = np.zeros_like(corner_image, dtype=np.uint8)
+    binary_image[corner_image == max_value] = 1 
+    return binary_image
+
