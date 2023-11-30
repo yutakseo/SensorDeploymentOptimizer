d6d801c0857da63d91a6abced599f4b01320ba51
yutak <soano2428@gmail.com>
2023-11-30 19:10:04 +0900

    23.11.30

--- __Main__.py
+++ __Main__.py
@@ -19,7 +19,10 @@ from greedy_algorithm import *
 
 result = corner_sensor_map(MAP, 40, 1,1)
 
-temp = greedy_algorithm(result, 40)
+#temp = greedy_algorithm(result, 40)
+#truncated_temp =[(51, 65), (52, 65), (37, 85), (82, 119)]
+#rectangle_temp = [(53, 56), (54, 56), (55, 56), (56, 56), (57, 56), (58, 56), (59, 56), (60, 56), (61, 56), (62, 56), (63, 56), (64, 56), (65, 56), (66, 56), (67, 56), (68, 56), (69, 56), (70, 56), (71, 56), (72, 56), (73, 56), (74, 56), (75, 56), (76, 56), (77, 56), (78, 56), (79, 56), (80, 56), (81, 56), (37, 85), (96, 87)]
+
 print(temp)
 for i in range(len(temp)):
     se = Sensor(MAP, temp[i], 50)
@@ -28,3 +31,4 @@ for i in range(len(temp)):
     
 show = VisualTool()
 show.show_jetmap("",MAP)
+
