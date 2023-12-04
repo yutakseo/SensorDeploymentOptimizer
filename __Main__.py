75f0de4e3ce305fb7b9d4e954a5b7dffab32a5bd
Yutak_Seo <soano2428@gmail.com>
2023-12-04 16:35:09 +0900

    23.12.04

--- __Main__.py
+++ __Main__.py
@@ -8,19 +8,19 @@ dir = ["MapData","VisualizationTool","SensorModule","Checker","OUTPUT"]
 for d in dir:
     sys.path.append(os.path.join(__file__,f"{d}"))
 
-from rectangle_140by140 import MAP
+from test_10by10 import MAP
 from VisualizationModule import *
 from corner_placement import *
 from greedy_algorithm import *
 
-'''
-temp = greedy_algorithm2(MAP, 40)
+
+temp = greedy_algorithm(MAP, 1)
 #truncated_temp =[(51, 65), (52, 65), (37, 85), (82, 119)]
 print(temp)
 for i in range(len(temp)):
-    se = Sensor(MAP, temp[i], 50)
+    se = Sensor(MAP, temp[i], 1)
     se.deploy_sensor()
-'''
+
 show = VisualTool()
 show.show_jetmap("",MAP)
 to_xlsx(MAP)
