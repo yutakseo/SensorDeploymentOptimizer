25a28ca2a1a2ab638fd1707fd091604ee1c2773b
Yutak_Seo <soano2428@gmail.com>
2024-02-26 17:43:47 +0900

    24.02.26 알고리즘 정리 파일 생성

--- __Main__.py
+++ __Main__.py
@@ -3,16 +3,16 @@ from toXLSX import *
 import numpy as np
 __file__ = os.getcwd()
 __root__ = os.path.dirname(__file__)
-dir = ["MapData","VisualizationTool","SensorModule","Checker","OUTPUT"]
+dir = ["MapData","Algorithm","VisualizationTool","SensorModule","Checker","OUTPUT"]
 
 for d in dir:
     sys.path.append(os.path.join(__file__,f"{d}"))
 
 from VisualizationModule import *
 from corner_map import *
-from greedy_algorithm import *
-from Greedy_Algorithm2 import *
-from genetic_algorithm2 import *
+from Algorithm.greedy_algorithm import *
+from Algorithm.Greedy_Algorithm2 import *
+from Algorithm.genetic_algorithm2 import *
 
 #!맵데이터 입력 : from [맵데이터_이름] <- 입력
 from truncated_140by140 import MAP
@@ -23,7 +23,7 @@ sensor_coverage = 10
 
 
 #!알고리즘 적용 : dst = [최적화 알고리즘(MAP, sensor_coverage)] <- 입력
-dst = 
+dst = 1
 a = SensorGA(20, 4, 4, MAP, 6,)
 a.run()
 
