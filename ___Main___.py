e3c2b08fdeb0f10d187c80bdf6e6394e86794728
Yutak_Seo <soano2428@gmail.com>
2024-03-30 23:16:53 +0900

    24.03.30

--- ___Main___.py
+++ ___Main___.py
@@ -11,7 +11,7 @@ from ComputerVisionModule.cv_detector import *
 from SensorModule.Sensor import Sensor
 from Algorithm.Greedy_Algorithm2 import *
 #사용할 건설현장 맵 선택
-from MapData.stair_140by140 import *
+from MapData.rectangle_140by140 import *
 #사용할 알고리즘 임포트 
 from Algorithm.genetic_algorithm import *
 #from greedy_algorithm2 import *
@@ -19,15 +19,14 @@ from Algorithm.genetic_algorithm import *
 
 
 #센서 커버리지 설정
-coverage = 20
+coverage = 25
 vis = VisualTool()
 #최외곽 센서 배치
 corner_position = ComputerVision(MAP).harris_corner(2, 3, 0.01)
 sensor = Sensor(MAP)
 for i in range(len(corner_position)):
     sensor.deploy(corner_position[i], coverage)
-
-
+MAP = sensor.result()
 
 #알고리즘 선택
 cord = sensor_GA(MAP, coverage, 10000).run()
@@ -39,7 +38,6 @@ for i in range(len(cord)):
     sensor.deploy(cord[i], coverage)
 MAP = sensor.result()
 print("배치된 센서 수 : ", len(cord))
-
 
 #결과출력
 vis.show_jetmap("",MAP)
