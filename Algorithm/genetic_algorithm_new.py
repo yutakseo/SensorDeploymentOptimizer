f8e03039fef14b58581989062032dd0ed3cf820a
yutak <soano2428@gmail.com>
2024-08-24 21:39:56 +0900

    24.08.24

--- Algorithm/genetic_algorithm_new.py
+++ Algorithm/genetic_algorithm_new.py
@@ -1,5 +1,5 @@
 import pygad
-import os, sys, random, copy
+import os, sys, random, copy, time
 import numpy as np
 from datetime import datetime
 
@@ -16,22 +16,22 @@ class sensor_GA:
         self.generations = generation
         self.feasible_positons = np.argwhere(self.map_data >= 1)
         
-        self.sensor_instance = Sensor(self.map_data)
         
-        self.__init__chromsome__ = np.random.choice([0,1], size=self.feasible_positons.shape[0], p=[0.75, 0.25])
-        self.num_of_parents_mating = 10
-        self.solutions_per_pop = 120
+        self.__init__chromsome__ = np.random.choice([0,1], size=self.feasible_positons.shape[0], p=[0.97, 0.03])
+        self.num_of_parents_mating = 20
+        self.solutions_per_pop = 100
         self.num_of_genes = len(self.feasible_positons)
         self.last_fitness = 0
         
         #초기 염색체 생성 함수
         function_inputs = self.__init__chromsome__
         #기대값 설정
-        desired_output = 1000
+        desired_output = 48
         #유전자 해범위 설정
         self.range_ben = [{"low": 0,"high":1.1} for i in range(self.num_of_genes)]
         
     def deploy_simulation(self, solution):
+        self.sensor_instance = Sensor(self.map_data)
         self.sensor_instance.create_circle(self.coverage)
         for i in range(self.num_of_genes):
             if solution[i] == 1:
@@ -43,12 +43,13 @@ class sensor_GA:
         numb_of_sensors = np.sum(solution == 1)
         numb_of_uncovered = np.sum(temp == 1)
         restrict_condition1 = self.num_of_genes - numb_of_uncovered
-        return restrict_condition1/numb_of_sensors
+        return restrict_condition1
         
     def on_generation(self, ga_instance):
         print("\nGeneration = {generation}".format(generation=ga_instance.generations_completed))
         print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1]))
         print("Change     = {change}".format(change=ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1] - self.last_fitness))
+        print("\n\n")
         self.last_fitness = ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1]
         return self.last_fitness
         
@@ -61,12 +62,12 @@ class sensor_GA:
                         gene_type = int,
                         gene_space = self.range_ben,
                         fitness_func = self.fitness_func,
-                        parent_selection_type="rank",
-                        crossover_type="scattered",
+                        parent_selection_type="sss",
+                        crossover_type="two_points",
                         mutation_type="adaptive",
-                        mutation_probability=[1, 0.7],
+                        mutation_probability=[0.9, 1],
                         on_generation = self.on_generation,
-                        stop_criteria=["reach_80.0", "saturate_500"],
+                        stop_criteria=["reach_48","saturate_500"],
                         parallel_processing=24)
         
         ga_instance.run()
@@ -87,5 +88,6 @@ class sensor_GA:
 
     
     
-test = sensor_GA(MAP2, 3, 100)
+test = sensor_GA(MAP2, 2, 1000)
+print("최종해",test.num_of_genes)
 test.run()
